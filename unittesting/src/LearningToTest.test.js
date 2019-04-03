1baf3afb9b14a79a83c4cd974e541f82b26d6bf0
chrisbrake <chris.brake@gmail.com>
2019-04-02 21:07:33 -0600

    Playing

---
+++ unittesting/src/LearningToTest.test.js
@@ -0,0 +1,19 @@
+//import { add, total } from './App';
+
+const add = jest.fn(() => 3);
+const total = jest.fn(() => '$25');
+
+// Unit test
+// It only tests one thing
+test('add', () => {
+    expect(add(1, 2)).toBe(3);
+    expect(add).toHaveBeenCalledTimes(1);
+    expect(add).toHaveBeenCalledWith(1, 2);
+    //expect(add(5, 2)).toBe(7);
+});
+
+// Integration tests 
+// Tests things working together
+test('total', () => {
+    expect(total(5, 20)).toBe('$25');
+});
